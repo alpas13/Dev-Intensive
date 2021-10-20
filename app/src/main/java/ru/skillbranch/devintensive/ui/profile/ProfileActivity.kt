deb95a47b409a32d641a8fdca52c778db9bb11a9
Alexey <alexey@Oleksiys-MBP.fritz.box>
2021-10-21 00:35:52 +0200

    Implemented repository validation

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -5,6 +5,8 @@ import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.EditText
@@ -44,6 +46,17 @@ class ProfileActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         viewModel.getProfileData().observe(this, Observer { updateUI(it) })
         viewModel.getTheme().observe(this, Observer { updateTheme(it) })
+        viewModel.getRepositoryError().observe(this, Observer { updateRepoError(it) })
+        viewModel.getIsRepoError().observe(this, Observer { updateRepository(it) })
+    }
+
+    private fun updateRepository(isError: Boolean) {
+        if (isError) et_repository.text.clear()
+    }
+
+    private fun updateRepoError(isError: Boolean) {
+        wr_repository.isErrorEnabled = isError
+        wr_repository.error = if (isError) "Невалидный адрес репозитория" else null
     }
 
     private fun updateTheme(mode: Int) {
@@ -75,10 +88,20 @@ class ProfileActivity : AppCompatActivity() {
         showCurrentMode(isEditMode)
 
         btn_edit.setOnClickListener {
+            viewModel.onRepoEditCompleted(wr_repository.isErrorEnabled)
+
             if (isEditMode) saveProfileInfo()
             isEditMode = !isEditMode
             showCurrentMode(isEditMode)
         }
+
+        et_repository.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+            override fun afterTextChanged(s: Editable?) {
+                viewModel.onRepositoryChanged(s.toString())
+            }
+        })
 
         btn_switch_theme.setOnClickListener {
             viewModel.switchTheme()
--- app/src/main/java/ru/skillbranch/devintensive/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/ProfileViewModel.kt
@@ -11,24 +11,29 @@ import ru.skillbranch.devintensive.repositories.PreferencesRepository
 /**
  * Created by Oleksiy Pasmarnov on 11.10.21
  */
-class ProfileViewModel: ViewModel() {
+class ProfileViewModel : ViewModel() {
     private val repository: PreferencesRepository = PreferencesRepository
     private val profileData = MutableLiveData<Profile>()
     private val appTheme = MutableLiveData<Int>()
+    private val repositoryError = MutableLiveData<Boolean>()
+    private val isRepoError = MutableLiveData<Boolean>()
 
     init {
-        Log.d("M_ProfileViewModel","init view model")
+        Log.d("M_ProfileViewModel", "init view model")
         profileData.value = repository.getProfile()
         appTheme.value = repository.getAppTheme()
     }
 
+    fun getIsRepoError(): LiveData<Boolean> = isRepoError
+
     override fun onCleared() {
         super.onCleared()
-        Log.d("M_ProfileViewModel","view model cleared")
+        Log.d("M_ProfileViewModel", "view model cleared")
     }
 
     fun getProfileData(): LiveData<Profile> = profileData
     fun getTheme(): LiveData<Int> = appTheme
+    fun getRepositoryError(): LiveData<Boolean> = repositoryError
 
     fun saveProfileData(profile: Profile) {
         repository.saveProfile(profile)
@@ -45,5 +50,40 @@ class ProfileViewModel: ViewModel() {
         repository.saveAppTheme(appTheme.value!!)
     }
 
+    fun onRepositoryChanged(repository: String) {
+        repositoryError.value = isValidateRepository(repository)
+    }
+
+
+    fun onRepoEditCompleted(isError: Boolean) {
+        isRepoError.value = isError
+    }
+
+    private fun isValidateRepository(repoText: String): Boolean {
+        val regexStr =
+            "^(https://)?(www\\.)?(github\\.com/)(?!(${getRegexExceptions()})(?=/|\$))[a-zA-Z\\d](?:[a-zA-Z\\d]|-(?=[a-zA-Z\\d])){0,38}(/)?$"
+        val regex = Regex(regexStr)
+
+        return (repoText.isNotEmpty() && !regex.matches(repoText))
+    }
+
+    private fun getRegexExceptions(): String {
+        val exceptions = arrayOf(
+            "enterprise",
+            "features",
+            "topics",
+            "collections",
+            "trending",
+            "events",
+            "marketplace",
+            "pricing",
+            "nonprofit",
+            "customer-stories",
+            "security",
+            "login",
+            "join"
+        )
+        return exceptions.joinToString("|")
+    }
 
 }
